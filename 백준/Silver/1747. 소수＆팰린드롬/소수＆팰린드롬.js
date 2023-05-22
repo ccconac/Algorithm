61b7c7e9fc27c23653977414f0c40c8e31202299
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-05-22 16:18:24 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 532 ms, Memory: 12828 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.js
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.js
@@ -2,33 +2,28 @@ const input = require('fs').readFileSync(0).toString().trim();
 
 let N = parseInt(input);
 
-const prime = (num) => {
-  if (num < 2) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const palindrome = (num) => {
-    const strNum = String(num);
-    const length = strNum.length;
-    for (let i = 0; i < length / 2; i++) {
-        if (strNum[i] !== strNum[length - 1 - i]) {
-            return false;
-        }
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
     }
     return true;
 }
 
-while (true) {
-    if (prime(N) && palindrome(N)) {
-      console.log(N);
-      break;
+const isPalindrome = (num) => {
+    const strNum = String(num);
+    const length = strNum.length;
+
+    for (let i = 0; i <= length / 2; i++) {
+        if (strNum[i] !== strNum[length - 1 - i]) return false;
+    }
+    return true;   
+}
+
+while(true) {
+    if (isPrime(N) && isPalindrome(N)) {
+        console.log(N);
+        break;
     }
     N++;
 }
