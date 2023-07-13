7e94aad72da4286b706bcd64647826a0f2bb7479
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-07-13 13:41:04 +0900

    [Silver V] Title: 너의 평점은, Time: 148 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.js
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -1,31 +1,25 @@
-const input = require('fs').readFileSync(0).toString().trim().split('\n');
+const input = require('fs').readFileSync(0).toString().trim().split('\n').map(v => v.split(' '));
 
-const arr = input.map(v => v.split(' '));
-const score = [];
-const grade = [];
-
-arr.forEach(v => {
-    if (v[2] !== 'P') {
-        score.push(parseInt(v[1]));
-        if (v[2] === 'A+') grade.push(4.5);
-        else if (v[2] === 'A0') grade.push(4.0);
-        else if (v[2] === 'B+') grade.push(3.5);
-        else if (v[2] === 'B0') grade.push(3.0);
-        else if (v[2] === 'C+') grade.push(2.5);
-        else if (v[2] === 'C0') grade.push(2.0);
-        else if (v[2] === 'D+') grade.push(1.5);
-        else if (v[2] === 'D0') grade.push(1.0);
-        else grade.push(0.0);
-    }  
-});
+const grade = {
+    'A+': 4.5,
+    'A0': 4.0,
+    'B+': 3.5,
+    'B0': 3.0,
+    'C+': 2.5,
+    'C0': 2.0,
+    'D+': 1.5,
+    'D0': 1.0,
+    'F': 0.0
+}
 
 let sum = 0;
 let scoreSum = 0;
 
-score.forEach((v, i) => {
-    sum += v * grade[i];
-    scoreSum += v;
-});
+for (let [sub, score, g] of input) {
+	if (g == "P") continue;
+	scoreSum += score * 1;
+	sum += score * grade[g];
+}
 
-const avg = sum / scoreSum;
+const avg = sum / scoreSum
 console.log(avg);
