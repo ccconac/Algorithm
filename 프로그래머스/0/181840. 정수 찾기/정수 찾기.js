dff15f5de1ab7b09d06e3ac2df5e154775c7df02
sohee <106158901+ccconac@users.noreply.github.com>
2024-03-10 00:07:43 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+    let answer = 0;
+    
+    if (num_list.includes(n)) {
+        answer = 1;
+    } else {
+        answer = 0;
+    }
+    
+    return answer;
+}
