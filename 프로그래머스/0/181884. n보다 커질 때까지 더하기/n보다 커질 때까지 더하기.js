e05428a1103ec885b3946fc462a4b8d9fdb62271
sohee <106158901+ccconac@users.noreply.github.com>
2024-03-11 23:57:00 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,11 +1,3 @@
 function solution(numbers, n) {
-  let answer = 0;
-    
-  for (let i = 0; i < numbers.length; i++) {
-    answer += numbers[i];
-      
-    if (answer > n) {
-      return answer;
-    }
-  }
+    return numbers.reduce((acc, curr) => (acc <= n) ? acc + curr : acc)
 }
