f0b65dab4f3d0c0c78ced00b54673bb4c56dc04b
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-01 23:21:34 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -0,0 +1,12 @@
+const [A, B, C] = require('fs').readFileSync(0).toString().split(' ').map(BigInt);
+
+const solve = (power) => {
+  if (power === 1n) return A % C;
+
+  const half = solve(power / 2n) % C;
+  if (power % 2n) return (half * half * (A % C)) % C;
+
+  return (half * half) % C;
+};
+
+console.log(solve(B).toString());
