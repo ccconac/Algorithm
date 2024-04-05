be137cf6678d619a1313056863d7c51f69fe043b
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-05 23:41:35 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,9 +1,3 @@
 function solution(a, b) {
-    let answer = 0;
-    const bigA = Number(a.toString() + b.toString());
-    const bigB = Number(b.toString() + a.toString());
-    
-    bigA > bigB ? answer = bigA : answer = bigB;
-    
-    return answer;
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
 }
