64ef9ca1528425364a42cfc5869097000e4c9675
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-09 23:14:32 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    const lower = my_string.toLowerCase();
-    
-    return lower.split('').sort().join('');
+    return [...my_string.toLowerCase()].sort().join('');
 }
