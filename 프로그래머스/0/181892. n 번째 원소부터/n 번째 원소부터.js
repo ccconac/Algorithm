6d965d028937ec7939a698d5221fa91509283ef1
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-09 22:40:24 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,9 +1,3 @@
 function solution(num_list, n) {
-    const answer = [];
-    
-    for (let i = n - 1; i < num_list.length; i++) {
-        answer.push(num_list[i]);
-    }
-    
-    return answer;
+  return num_list.slice(n - 1);
 }
