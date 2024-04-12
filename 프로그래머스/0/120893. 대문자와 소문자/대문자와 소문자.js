a7d6850b019a8c7525b5acd5cf336cfeb36a463a
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-13 00:00:59 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('');
+    return [...my_string].map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('');
 }
