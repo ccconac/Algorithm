cbeec6628a9ad4c8fcdca8da8d6a4ee0dfa4270f
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-12 23:54:04 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.27 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,20 +1,10 @@
+const operations = {
+  w: (n) => n + 1,
+  s: (n) => n - 1,
+  d: (n) => n + 10,
+  a: (n) => n - 10,
+};
+
 function solution(n, control) {
-    [...control].forEach(value => {
-        switch (value) {
-            case 'w':
-                n += 1;
-                break;
-            case 's':
-                n -= 1;
-                break;
-            case 'd':
-                n += 10;
-                break;
-            case 'a':
-                n -= 10;
-                break;
-        }
-    })
-    
-    return n;
+  return [...control].reduce((prev, op) => operations[op](prev), n);
 }
