5bcf4feba066418723799869b8c41ff377b79e43
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-16 03:27:44 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,10 +1,4 @@
 function solution(num_list) {
-    const answer = [...num_list];
-    const prevNumber = num_list[num_list.length - 2];
-    const lastNumber = num_list[num_list.length - 1];
-    
-    if (lastNumber > prevNumber) answer.push(lastNumber - prevNumber);
-    if (lastNumber <= prevNumber) answer.push(lastNumber * 2);
-    
-    return answer;
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? (a - b) : a * 2];
 }
