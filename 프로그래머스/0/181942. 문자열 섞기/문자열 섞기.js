774dddf29334a6645d8fa209b6b7ced215034bc9
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-20 00:22:36 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,3 @@
 function solution(str1, str2) {
-    let answer = '';
-    
-    for (let i = 0; i < str1.length; i++) {
-        answer += `${str1[i]}${str2[i]}`;
-    }
-    
-    return answer;
+    return [...str1].map((value, index) => value + str2[index]).join('');
 }
