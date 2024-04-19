4deefe58afc9f96d7a10776ef2ac5374aa657e88
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-20 00:32:13 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,7 +1,11 @@
+const operations = {
+    '<=': (n, m) => n <= m,
+    '>=': (n, m) => n >= m,
+    '<!': (n, m) => n < m,
+    '>!': (n, m) => n > m,
+}
+
 function solution(ineq, eq, n, m) {
-    if (eq === '=' && n === m) return 1;
-    if (ineq === '<' && n < m) return 1;
-    if (ineq === '>' && n > m) return 1;
-    
-    return 0;
+    const op = operations[ineq + eq];
+    return Number(op(n, m));
 }
