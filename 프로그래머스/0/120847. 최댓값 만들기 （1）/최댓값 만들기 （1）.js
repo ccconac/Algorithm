3b2b6479f23fdd57c0cc23468c29989895314b38
sohee <106158901+ccconac@users.noreply.github.com>
2024-04-30 22:14:03 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,8 +1,5 @@
 function solution(numbers) {
     numbers.sort((a, b) => b - a);
     
-    const max1 = numbers[0];
-    const max2 = numbers[1];
-    
-    return max1 * max2;
+    return numbers[0] * numbers[1];
 }
