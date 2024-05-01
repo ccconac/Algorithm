fe923e1ceb9ac72e35a4351c75ec820c80a5c1c2
sohee <106158901+ccconac@users.noreply.github.com>
2024-05-01 23:02:02 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    const numbers = my_string.match(/[0-9]+/g);
-    
-    return numbers ? numbers.map(Number).reduce((acc, curr) => acc + curr, 0) : 0;
+  return my_string.split(/\D+/).reduce((acc, curr) => acc + Number(curr), 0);
 }
