202630b663a10da97507d27e8d81957a00296b46
sohee <106158901+ccconac@users.noreply.github.com>
2024-05-10 23:31:09 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,10 +1,10 @@
 function solution(babbling) {
-    let answer = 0;
-    
-    babbling.forEach(value => {
-        if (value.replace('aya', ' ').replace('ye', ' ').replace('woo', ' ')
-            .replace('ma', ' ').trim() === '') answer += 1;
-    })
-    
-    return answer;
+  const REGEX = /^(aya|ye|woo|ma)+$/;
+  let answer = 0;
+
+  babbling.forEach(word => {
+    if (REGEX.test(word)) answer += 1;  
+  })
+
+  return answer;
 }
