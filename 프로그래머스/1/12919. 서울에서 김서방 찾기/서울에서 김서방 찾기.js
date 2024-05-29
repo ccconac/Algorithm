c091f36032f6bb6809efc330dabcfcff5bafd6c8
sohee <106158901+ccconac@users.noreply.github.com>
2024-05-29 23:17:55 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,8 +1,3 @@
 function solution(seoul) {
-    let location = 0;
-    seoul.forEach((person, index) => {
-        if (person === 'Kim') location = index;
-    })
-    
-    return `김서방은 ${location}에 있다`
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`
 }
