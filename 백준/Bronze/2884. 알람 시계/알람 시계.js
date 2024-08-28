9cd89b0e4e8882e110ca9c40ae5c6931881b78ce
sohee <106158901+ccconac@users.noreply.github.com>
2024-08-28 15:03:17 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,8 @@
+const [H, M] = require('fs').readFileSync(0).toString().split(' ').map(Number);
+
+if (M >= 45) {
+	console.log(H, M - 45);
+} else if (M < 45) {
+	if (H === 0) console.log(23, M + 15);
+	else console.log(H - 1, M + 15);
+}
