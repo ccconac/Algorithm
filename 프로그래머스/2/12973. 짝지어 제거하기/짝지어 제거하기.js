fbfe1f1fd06d42db69304eeb65f43e1c478feedf
sohee <106158901+ccconac@users.noreply.github.com>
2025-01-23 11:50:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 67.85 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s.charAt(i);
+        
+        if (char === stack[stack.length - 1]) stack.pop();
+        else stack.push(char);
+    }
+    
+    return stack.length ? 0 : 1;
+}
