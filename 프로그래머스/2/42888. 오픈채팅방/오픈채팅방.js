c4801e4f71361e1920239b1831038732ec3678cb
sohee <106158901+ccconac@users.noreply.github.com>
2025-02-02 01:07:17 +0900

    [level 2] Title: 오픈채팅방, Time: 155.02 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,24 +1,19 @@
 function solution(record) {
-    const userInfo = {};
-    const openchatLog = [];
-    
-    record.forEach((log) => {
-        const [command, uid, nickname] = log.split(' ');
-        
-        if (command === 'Enter') {
-            userInfo[uid] = nickname;
-            openchatLog.push(`${uid}님이 들어왔습니다.`);
-        } else if (command === 'Leave') {
-            openchatLog.push(`${uid}님이 나갔습니다.`);
-        } else if (command === 'Change') {
-            userInfo[uid] = nickname;
-        }
-    });
-    
-    const answer = openchatLog.map((log) => {
-        const uid = log.split('님이')[0];
-        return log.replace(uid, userInfo[uid]);
-    });
-    
-    return answer;
+  const userInfo = {};
+  const openchatLog = [];
+  const commandMapping = {
+    Enter: '님이 들어왔습니다.',
+    Leave: '님이 나갔습니다.',
+  };
+
+  record.forEach((v) => {
+    const [command, id, nickname] = v.split(' ');
+
+    if (command !== 'Change') openchatLog.push([command, id]);
+    if (nickname) userInfo[id] = nickname;
+  });
+
+  return openchatLog.map(([command, uid]) => {
+    return `${userInfo[uid]}${commandMapping[command]}`;
+  });
 }
