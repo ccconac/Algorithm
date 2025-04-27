f7731f0551d4c238fc98da5c158871d671c87fe5
sohee <106158901+ccconac@users.noreply.github.com>
2025-04-27 15:14:19 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,8 @@
 function solution(a, b) {
-    const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const dayOfWeek = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
     const days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     
-    let day = b + 4;
+    let day = b - 1;
     
     for (let i = 0; i < a - 1; i += 1) day += days[i];
     
