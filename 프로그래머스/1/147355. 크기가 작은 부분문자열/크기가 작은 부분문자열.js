a695b6354ac1e643e821e69b72bc59de473235cb
sohee <106158901+ccconac@users.noreply.github.com>
2025-04-29 20:23:46 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 22.50 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,19 +1,14 @@
 function solution(t, p) {
     // 숫자로 이루어진 문자열 t와 p  
-    const arr = [];
+    // t에서 p와 길이가 같은 부분 문자열 중  
+    // 부분 문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 "횟수"
     let answer = 0;
     
-    for (let i = 0; i < t.length; i += 1) {
-        arr.push(t.slice(i, i + p.length));
+    for (let i = 0; i <= t.length - p.length; i += 1) {
+        const partialString = t.slice(i, i + p.length);
+        
+        if (+partialString <= +p) answer += 1;
     }
-    
-    // t에서 p와 길이가 같은 부분 문자열 중  
-    const numbers = arr.filter(number => number.length === p.length);
-    
-    // 부분 문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 "횟수"
-    numbers.forEach(number => {
-        if (Number(number) <= Number(p)) answer += 1;
-    });
     
     return answer;
 }
