8e07588b632cb10765e17e34a67d0275b7eb6af1
sohee <106158901+ccconac@users.noreply.github.com>
2025-04-30 14:26:44 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -4,12 +4,10 @@ function solution(num) {
     // 1-1. 입력된 수가 짝수라면 2로 나눔
     // 1-2. 홀수라면 3을 곱하고 1을 더함
     // 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복
-    while (num !== 1) {
-        if (answer === 500) return -1;
-        
+    while (num !== 1 && answer !== 500) {
         num = num % 2 ? num * 3 + 1 : num / 2;
         answer += 1;
     }
     
-    return answer;
+    return num === 1 ? answer : -1;
 }
