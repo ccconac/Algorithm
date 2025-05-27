21be27317d1c6aeaea035640a09a784d51f72435
sohee <106158901+ccconac@users.noreply.github.com>
2025-05-27 15:59:34 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -6,8 +6,8 @@ function solution(number) {
     
     let answer = 0;
     
-    for (let i = 0; i < number.length; i += 1) {
-        for (let j = i + 1; j < number.length; j += 1) {
+    for (let i = 0; i < number.length - 2; i += 1) {
+        for (let j = i + 1; j < number.length - 1; j += 1) {
             for (let k = j + 1; k < number.length; k += 1) {
                 if (number[i] + number[j] + number[k] === 0) answer += 1;
             }
