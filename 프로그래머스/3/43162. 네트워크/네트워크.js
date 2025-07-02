f4ca0a1836a0afc8dad22afb4db6547cb67cd752
sohee <106158901+ccconac@users.noreply.github.com>
2025-07-02 13:57:19 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,23 @@
+function dfs(computers, visited, node) {
+    visited[node] = true;
+    
+    for (let i = 0; i < computers[node].length; i += 1) {
+        if (computers[node][i] && !visited[i]) {
+            dfs(computers, visited, i);
+        }
+    }
+}
+
 function solution(n, computers) {
-    const visited = new Array(n).fill(0);
-    let count = 0;
-
-    function dfs(node) {
-        visited[node] = 1;
-
-        for (let i = 0; i < n; i++) {
-            if (!visited[i] && computers[node][i]) dfs(i);
-        }
-    }
-
-    for (let i = 0; i < n; i++) {
+    let answer = 0;
+    const visited = Array(n).fill(false);
+    
+    for (let i = 0; i < n; i += 1) {
         if (!visited[i]) {
-            dfs(i);
-            count += 1;
+            dfs(computers, visited, i);
+            answer += 1;
         }
     }
-
-    return count;
+    
+    return answer;
 }
