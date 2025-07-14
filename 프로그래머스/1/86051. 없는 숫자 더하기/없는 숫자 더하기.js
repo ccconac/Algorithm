cc250996cf730c2cdfe747293f37817da70d6387
sohee <106158901+ccconac@users.noreply.github.com>
2025-07-14 14:55:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,3 @@
 function solution(numbers) {
-    const hideNumbers = [];
-    
-    for (let i = 0; i < 10; i += 1) {
-        if (!numbers.includes(i)) hideNumbers.push(i);
-    }
-    
-    return hideNumbers.reduce((acc, curr) => acc + curr, 0);
+    return 45 - numbers.reduce((acc, curr) => acc + curr, 0);
 }
