be09764246982c747250d258ee38bb91bcc7b0ba
sohee <106158901+ccconac@users.noreply.github.com>
2025-07-22 16:50:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 34.00 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,35 +1,32 @@
 function solution(s) {
-    const n = s.length; 
-    let answer = 0;
-    
-    for (let i = 0; i < n; i++) {
-        const stack = [];
-        let isCorrect = true;
-        
-        for (let j = 0; j < n; j++) {
-            const c = s[(i + j) % n];
-            
-            if (c === '(' || c === '{' || c === '[') stack.push(c);
-            else {
-                if (!stack.length) {
-                    isCorrect = false;
-                    break;
-                }
-                
-                const top = stack[stack.length - 1];
-                
-                if (c === ')' && top === '(') stack.pop();
-                else if (c === '}' && top === '{') stack.pop();
-                else if (c === ']' && top === '[') stack.pop();
-                else {
-                    isCorrect = false;
-                    break;
-                }
-            }
+  const n = s.length;
+  const mapping = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
+
+  let answer = 0;
+
+  for (let i = 0; i < n; i += 1) {
+    const stack = [];
+    let isCorrect = true;
+
+    for (let j = 0; j < n; j += 1) {
+      const c = s[(i + j) % n];
+
+      if (c === '(' || c === '{' || c === '[') stack.push(c);
+      else {
+        const top = stack.pop();
+        if (top !== mapping[c]) {
+          isCorrect = false;
+          break;
         }
-        
-        if (isCorrect && !stack.length) answer += 1;
+      }
     }
-    
-    return answer;
+
+    if (isCorrect && !stack.length) answer += 1;
+  }
+
+  return answer;
 }
