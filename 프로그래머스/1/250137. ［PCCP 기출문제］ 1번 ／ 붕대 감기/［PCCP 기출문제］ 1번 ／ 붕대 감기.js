4c7251a00b5618ca8a1f2518bdc704868f018784
sohee <106158901+ccconac@users.noreply.github.com>
2025-07-29 23:44:43 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,35 @@
+function solution(bandage, health, attacks) {
+    let answer = 0;
+    
+    let attackMap = new Map(attacks);
+    let lastAttack = Math.max(...attackMap.keys());
+    
+    const MAX = health;
+    let hp = health;
+    let success = 0;
+    
+    for (let i = 1; i <= lastAttack; i++) {
+        if (hp <= 0) break;
+
+        if (!attackMap.has(i)) {
+             success += 1;
+            
+            if (success === bandage[0]) {
+                hp += bandage[2];
+                success = 0;
+            }
+            
+            hp += bandage[1];
+        } else {
+            let damage = attackMap.get(i);
+            hp -= damage;
+            success = 0;
+        }
+
+        if (hp > MAX) hp = MAX;
+    }
+    
+    answer = hp <= 0 ? -1 : hp;
+    
+    return answer;
+}
