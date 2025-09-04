426ccaae592442c0635c2b29afb95c613413f332
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-04 12:55:40 +0900

    [level 2] Title: 행렬의 곱셈, Time: 14.21 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,5 +1,5 @@
 function solution(arr1, arr2) {
-    const answer = Array.from({length: arr1.length}, () => Array.from({length: arr2[0].length}).fill(0));
+    const answer = Array.from({ length: arr1.length }, () => Array.from({ length: arr2[0].length }).fill(0));
     
     for (let i = 0; i < arr1.length; i += 1) {
         for (let j = 0; j < arr2[0].length; j += 1) {
@@ -10,5 +10,4 @@ function solution(arr1, arr2) {
     }
     
    return answer;
-
 }
