9e012290dd91c8d25795600ca074a87934dfe171
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-08 15:26:50 +0900

    [level 2] Title: 짝지어 제거하기, Time: 73.64 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,14 @@
 function solution(s) {
+    // 1. 같은 알파벳이 2개 붙어 있는 짝을 찾음
+    // 2. 둘을 제거한 뒤 앞뒤로 문자열을 이어 붙임
+    // 3. 위 과정을 반복해서 문자열을 모두 제거한다면 짝지어 제거하기 종료
+    
     const stack = [];
-    const n = s.length;
     
-    if (n % 2) return 0;
-    
-    for (const c of s) {
-        if (c === stack[stack.length - 1]) stack.pop();
+    [...s].forEach((c) => {
+        if (stack[stack.length - 1] === c) stack.pop();
         else stack.push(c);
-    }
+    })
     
     return stack.length ? 0 : 1;
 }
