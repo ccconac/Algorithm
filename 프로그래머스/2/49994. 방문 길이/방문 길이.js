d6151c5009cac8df01f4673540b0afc03ea6ce25
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-08 14:07:47 +0900

    [level 2] Title: 방문 길이, Time: 0.89 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,30 +1,29 @@
 function solution(dirs) {
+    // 1. 캐릭터 방향 객체 생성
     const move = {
         U: [0, 1],
         D: [0, -1],
         L: [-1, 0],
-        R: [1, 0],
+        R: [1, 0]
     };
-
+    
+    // 2. 캐릭터의 방문 경로를 담을 객체 생성 (중복 비허용)
     const visited = new Set();
     let x = 0, y = 0;
-
-    for (let dir of dirs) {
+    
+    for (const dir of dirs) {
         const [dx, dy] = move[dir];
         const nx = x + dx;
         const ny = y + dy;
-
-        if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
-
-        const path = `${x},${y},${nx},${ny}`;
-        const reversePath = `${nx},${ny},${x},${y}`;
         
-        visited.add(path);
-        visited.add(reversePath);
+        if (nx > 5 || nx < -5 || ny > 5 || ny < -5) continue;
+        
+        visited.add(`${nx}${ny}${x}${y}`);
+        visited.add(`${x}${y}${nx}${ny}`);
         
         x = nx;
         y = ny;
     }
-
+    
     return visited.size / 2;
 }
