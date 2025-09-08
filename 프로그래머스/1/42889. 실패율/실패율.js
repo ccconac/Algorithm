391c9cc2aa6536f9028212081de2dea5fa84497d
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-08 13:06:59 +0900

    [level 1] Title: 실패율, Time: 24.05 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,35 +1,31 @@
 function solution(N, stages) {
-    // 전체 스테이지 개수 N
-    // 사용자가 현재 멈춰 있는 스테이지의 번호가 담긴 배열 stages
-    // 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
-    // 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨 있는 배열 반환 
+    // 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+    // 전체 스테이지 개수 = N, 현재 멈춰 있는 스테이지의 번호가 담긴 배열 stages
     
-    // 1. 스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수 배열에 담기 (현재 머물러 있는 도전자)
-    const challenger = Array.from({length: N + 2}).fill(0);
-    stages.forEach(stage => challenger[stage] += 1);
+    // 1. 현재 스테이지에 몇 명의 도전자가 있는지 배열에 카운트
+    const challenger = Array.from({ length: N + 2 }).fill(0);
+    stages.forEach((stage) => challenger[stage] += 1);
     
-    // 실패율을 담는 객체 필요 (레벨에 해당하는 실패율을 알기 위해 객체 정의)
+    // 2. 실패율 객체 생성
     const failRatio = {};
-    let total = stages.length;
+    let total = stages.length; 
     
-    // 2. 스테이지에 도달한 플레이어 수 (현재 머물러 있는 사람보다 큰 사람들 다 담으면 됨)
-    for (let i = 1; i <= N; i += 1) {
-        // 아직 클리어하지 못한 플레이어의 수가 0인 경우 실패율은 0 => 5번 스테이지 예시
+    // 현재 스테이지의 실패율을 구하는 거니까 
+    // 스테이지 2라고 하면 클리어하지 못한 사람 3명 / 총 도전자 7명
+    for (let i = 1; i <= N; i++) {
+        // 스테이지에 도달한 유저가 0일 경우 실패율은 0
         if (challenger[i] === 0) {
-            failRatio[i] = 0; 
+            failRatio[i] = 0;
             continue;
         }
         
-        // 3. 실패율 계산
-        // 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
         failRatio[i] = challenger[i] / total;
         
-        // 스테이지에 도달한 플레이어 수에서 아직 클리어하지 못한 플레이어 수를 마지막에 빼 줌
         total -= challenger[i];
     }
     
-    // 실패율이 높은 스테이지부터 내림차순으로 정렬
     const answer = Object.entries(failRatio).sort((a, b) => b[1] - a[1]);
     
-    return answer.map(value => Number(value[0]));
+    return answer.map((value) => Number(value[0]));
+    
 }
