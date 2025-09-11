d8331cee52eea30c5b81c84d2df0be3bfb4218fb
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-11 11:37:17 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,27 @@
+function solution(board, moves) {
+    // board 변환
+    const lanes = [...Array(board[0].length)].map(() => []);
+    
+    for (let i = board.length - 1; i >= 0; i--) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j]) lanes[j].push(board[i][j]);
+        }
+    }
+    
+    const bucket = [];
+    let answer = 0;
+    
+    for (let i = 0; i < moves.length; i++) {
+        if (lanes[moves[i] - 1].length) {
+            const doll = lanes[moves[i] - 1].pop();
+            
+            if (bucket.length && doll === bucket[bucket.length - 1]) {
+                bucket.pop();
+                answer += 2;
+            } else bucket.push(doll);
+            
+        }
+    }
+    
+    return answer;
+}
