bb0e9cd7fdbd2159de5c2736d0c8618d5b006c47
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-15 07:51:29 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,15 @@
 function solution(progresses, speeds) {
-    // 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됨
-    // 배포는 하루에 한 번 하루의 끝
-    
-    // 93%에 1%씩이면 7일, 30%에 30%씩이면 3일
-    // 100 - 93 / 1 = 7, 100 - 30 / 30 = 3 (올림)
-    const answer = [];
-    const deployDays = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    
-    let i = 0;
-    while (i < deployDays.length) {
-        let functionCount = 1;
-        let currentDeployDay = deployDays[i];
-        let j = i + 1;
-        
-        while (j < deployDays.length && deployDays[j] <= currentDeployDay) {
-            functionCount += 1;
-            j += 1;
+    let answer = [1];
+    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    let maxDay = days[0];
+
+    for(let i = 1, j = 0; i< days.length; i++){
+        if (days[i] <= maxDay) answer[j] += 1;
+        else {
+            maxDay = days[i];
+            answer[++j] = 1;
         }
-        
-        answer.push(functionCount);
-        i = j;
     }
-    
+
     return answer;
 }
