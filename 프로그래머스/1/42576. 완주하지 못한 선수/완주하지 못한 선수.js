5b1b610fb85cd60e73612ddaea48ed076b20f1f6
sohee <106158901+ccconac@users.noreply.github.com>
2025-09-16 14:09:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.74 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,16 @@
 function solution(participant, completion) {
-    // 한 명의 선수를 제외하고 모든 선수가 마라톤 완주
-    // 완주하지 못한 선수의 이름을 반환
+    const obj = {};
     
-    // 1. participant와 completion 비교
-    // 1-1. participant 돌면서 completion includes로 찾기
-    // 1-2. 없으면 반환
+    for (const p of participant) {
+        if (obj[p]) obj[p] += 1;
+        else obj[p] = 1;
+    }
     
-    participant = participant.sort();
-    completion = completion.sort();
+    for (const c of completion) {
+        obj[c] -= 1;
+    }
     
-    for (let i = 0; i < participant.length; i += 1) {
-        if (participant[i] !== completion[i]) return participant[i];
+    for (const key in obj) {
+        if (obj[key]) return key;
     }
 }
