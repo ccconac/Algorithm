16c1e97ce6ff2d516504b176885563b661805718
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-03 12:04:46 +0900

    [level 1] Title: 모의고사, Time: 2.48 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,22 @@
 function solution(answers) {
-  const person1 = [1, 2, 3, 4, 5];
-  const person2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  const scores = [0, 0, 0];
-
-  answers.forEach((answer, index) => {
-    if (person1[index % person1.length] === answer) scores[0] += 1;
-    if (person2[index % person2.length] === answer) scores[1] += 1;
-    if (person3[index % person3.length] === answer) scores[2] += 1;
-  });
-
-  const maxScore = Math.max(...scores);
-  const answer = [];
+    const first = [1, 2, 3, 4, 5];
+    const second = [2, 1, 2, 3, 2, 4, 2, 5];
+    const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-  scores.forEach((score, index) => {
-    if (score === maxScore) answer.push(index + 1);
-  });
-
-  return answer;
+    const scores = [0, 0, 0];
+    
+    answers.forEach((answer, index) => {
+        if (answer === first[index % first.length]) scores[0] += 1;
+        if (answer === second[index % second.length]) scores[1] += 1;
+        if (answer === third[index % third.length]) scores[2] += 1;
+    });
+    
+    const maxScore = Math.max(...scores);
+    const answer = [];
+            
+    scores.forEach((score, index) => {
+        if (maxScore === score) answer.push(index + 1);
+    });
+    
+    return answer;
 }
