a660f08e76da3539cf1884e6f893d59925f3ccee
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-03 14:19:37 +0900

    [level 2] Title: 방문 길이, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,23 +1,20 @@
 function solution(dirs) {
-    // 1. 방향 객체 설정
     const move = {
-        'U': [0, 1],
-        'D': [0, -1],
-        'R': [1, 0],
-        'L': [-1, 0]
+        U: [0, 1],
+        D: [0, -1],
+        L: [-1, 0],
+        R: [1, 0]
     };
     
     const visited = new Set();
     let x = 0, y = 0;
     
-    // 좌표 평면의 경계를 넘어가는 명령어는 무시
-    // 처음 걸어본 길의 길이 구하기
     for (const dir of dirs) {
         const [dx, dy] = move[dir];
         const nx = x + dx;
         const ny = y + dy;
         
-        if (nx > 5 || nx < -5 || ny < -5 || ny > 5) continue;
+        if (nx > 5 || nx < -5 || ny > 5 || ny < -5) continue;
         
         visited.add(`${nx}${ny}${x}${y}`);
         visited.add(`${x}${y}${nx}${ny}`);
