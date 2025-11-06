be9ad35532b8e047bab02b9e32076dc221994cfc
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-06 22:46:27 +0900

    [level 2] Title: 주식가격, Time: 4.84 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,21 +1,20 @@
 function solution(prices) {
     const n = prices.length;
-    const answer = Array.from(n).fill(0);
-    
+    const answer = Array(n).fill(0);
     const stack = [0];
     
     for (let i = 1; i < n; i++) {
-        // 가격이 떨어졌을 때
+        // 주식 가격 내려가는 경우
         while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
             const top = stack.pop();
             answer[top] = i - top;
         }
         
-        // 가격이 떨어지지 않았을 때
+        // 주식 가격 유지되는 경우
         stack.push(i);
     }
     
-    // 종료될 때까지 가격이 떨어지지 않은 주가 
+    // 끝날 때까지 떨어지지 않은 주가
     while (stack.length) {
         const top = stack.pop();
         answer[top] = n - top - 1;
