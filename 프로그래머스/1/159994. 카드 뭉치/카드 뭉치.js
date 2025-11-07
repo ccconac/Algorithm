c404d0c6ea1723dc57aeaf79f3c0f5a84ce4637e
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-08 05:28:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,20 @@
 function solution(cards1, cards2, goal) {
-    let j = 0;
-    let k = 0;
-    
-    for (let i = 0; i < goal.length; i++) {
-        if (cards1[j] === goal[i]) j++;
-        else if (cards2[k] === goal[i]) k++;
-        else return 'No';
-    }
-    
-    return 'Yes';
+  let j = 0;
+  let k = 0;
+
+  for (let i = 0; i < goal.length; i++) {
+    if (cards1[j] === goal[i]) j++;
+    else if (cards2[k] === goal[i]) k++;
+    else return 'No';
+  }
+
+  return 'Yes';
 }
+
+console.log(
+  solution(
+    ['i', 'drink', 'water'],
+    ['want', 'to'],
+    ['i', 'want', 'to', 'drink', 'water']
+  )
+);
