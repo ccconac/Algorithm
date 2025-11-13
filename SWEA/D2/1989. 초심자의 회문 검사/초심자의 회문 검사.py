021c067c2a05ae90c9daaf20f9df8ef365cbd1fb
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-13 23:34:19 +0900

    [D2] Title: 초심자의 회문 검사, Time: 59 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,11 +1,10 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
-    word = input()
-    word_length = len(word) - 1
-    # 회문이면 1, 아니라면 0 출력
-    
+    string = input()
     answer = 0
-    if word == word[::-1]: answer =1
-        
+
+    if string == string[::-1]:
+        answer = 1
+
     print(f'#{test_case} {answer}')
