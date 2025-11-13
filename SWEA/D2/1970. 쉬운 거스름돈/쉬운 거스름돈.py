e54f0d380e8c9be0e7ff99c210d28624c73d660a
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-13 23:49:33 +0900

    [D2] Title: 쉬운 거스름돈, Time: 60 ms, Memory: 51,584 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -4,11 +4,11 @@ for test_case in range(1, T + 1):
     money = int(input())
     bills = {50000: 0, 10000: 0, 5000: 0, 1000: 0, 500: 0, 100: 0, 50: 0, 10: 0}
 
-    for bill in bills.keys():
-        if money >= bill:
-            count = money // bill 
-            bills[bill] = count 
-            money %= bill 
-	
-    print(f"#{test_case}")
-    print(" ".join(map(str,list(bills.values()))))
+    for bill in bills:
+        bills[bill] += money // bill
+        money %= bill
+
+    print(f'#{test_case}')
+    for count in bills.values():
+        print(count, end=' ')
+    print()
