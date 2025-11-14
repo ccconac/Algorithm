1a288bde9f2ea6665ce2cbf30f6a175d55a711e9
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-14 18:31:30 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -0,0 +1,24 @@
+def solution():
+    dp[0][0][1] = 1
+    for i in range(2, N):
+        if house[0][i] == 0:
+            dp[0][0][i] = dp[0][0][i - 1]
+
+    for row in range(1, N):
+        for col in range(1, N):
+            # 대각선의 경우 현재 좌표랑 (상, 좌) 비어 있어야 함
+            if house[row][col] == 0 and house[row][col - 1] == 0 and house[row - 1][col] == 0:
+                dp[1][row][col] = dp[0][row - 1][col - 1] + dp[1][row - 1][col - 1] + dp[2][row - 1][col - 1]
+
+            # 가로의 경우 현재 좌표만 비어 있으면 됨
+            # 세로의 경우 현재 좌표만 비어 있으면 됨
+            if house[row][col] == 0:
+                dp[0][row][col] = dp[0][row][col - 1] + dp[1][row][col - 1]
+                dp[2][row][col] = dp[1][row - 1][col] + dp[2][row - 1][col]
+
+    print(sum(dp[i][N - 1][N - 1] for i in range(3)))
+
+N = int(input())
+house = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+solution()
