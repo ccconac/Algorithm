28c929cc0e486471ece2777fc445ed676a0feea7
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-15 21:21:16 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 숫자 카드, Time: 57 ms, Memory: 53,888 KB -BaekjoonHub

--- SWEA/D2/4834. ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
+++ SWEA/D2/4834. ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
@@ -2,21 +2,18 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     N = int(input())
-    cards = list(map(int, input()))
-	# 0 ~ 9까지 적힌 카드 N장
-    # 가장 많은 카드에 적힌 숫자, 카드 몇 장
-    # 단, 카드 장수가 같을 때는 적힌 숫자가 큰 쪽을 출력한다.
-    card_count = [0] * 10
-    
-    for i in range(N):
-        card_count[cards[i]] += 1
-    
-    max_count = max(card_count)
-    card_number = 0
-    
+    A = list(map(int, input().strip()))
+    cards = [0] * 10
+
+    for i in A:
+        cards[i] += 1
+
+    max_count = max(cards)
+    max_value = 0
+
     for i in range(9, -1, -1):
-        if max_count == card_count[i]:
-            card_number = i
+        if cards[i] == max_count:
+            max_value = i
             break
-    
-    print(f'#{test_case} {card_number} {max_count}')
+
+    print(f'#{test_case} {max_value} {max_count}')
