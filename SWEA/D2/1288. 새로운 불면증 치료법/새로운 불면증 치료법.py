8052ed700bb61b8a6bbd1071bedf33581c231e8a
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-15 23:24:26 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 70 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -3,13 +3,18 @@ T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     answer = set()
-    
+
+    # N의 배수 번호인 양 (N, 2N, ..., kN)
+    # 이전에 셌던 번호들의 각 자리수에서 0-9까지 모든 숫자를 보는 경우
+
     count = 1
     while True:
-        for i in str(N * count): 
+        for i in str(N * count):
             answer.add(i)
-            
-        if len(answer) == 10: break  
+
+        if len(answer) == 10:
+            break
+
         count += 1
-    
-    print(f'#{test_case} {N * count}')
+
+    print(f'#{test_case} {count * N}')
