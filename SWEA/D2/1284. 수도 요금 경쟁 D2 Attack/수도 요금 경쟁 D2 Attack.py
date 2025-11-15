2d0b57e98388801a455594785141606556dde325
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-15 23:11:57 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 59 ms, Memory: 52,224 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -2,19 +2,18 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     P, Q, R, S, W = map(int, input().split())
-    
-    # A사: 1L -> P
-    # B사: R 이하: Q, R 초과: 1L -> S
-	# 종민이가 사용하는 수도 W(L)
-    # 요금 출력
-    
+
     A = W * P
     B = 0
-    result = 0
-    if W <= R: B = Q
-    else: B = Q + (W - R)*S
-        
-    if A > B: result = B
-    else: result = A
-        
-    print('#{} {}'.format(test_case, result))
+
+    # A: P * W
+    # B: W <= R: Q, W > R: Q + (W - R) * S
+    # 요금이 더 저렴한 회사의 요금 출력
+
+    if W <= R:
+        B = Q
+    else:
+        B = Q + (W - R) * S
+
+    answer = min(A, B)
+    print(f'#{test_case} {answer}')
