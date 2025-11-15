9390a9b462ef3c200e7e3ee65e4cc9706d3be517
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-15 21:05:12 +0900

    [D2] Title: [파이썬 S/W 문제해결 구현] 1일차 - 이진수, Time: 59 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D2/5185. ［파이썬 S／W 문제해결 구현］ 1일차 － 이진수/［파이썬 S／W 문제해결 구현］ 1일차 － 이진수.py
+++ SWEA/D2/5185. ［파이썬 S／W 문제해결 구현］ 1일차 － 이진수/［파이썬 S／W 문제해결 구현］ 1일차 － 이진수.py
@@ -1,11 +1,10 @@
 T = int(input())
 
-for test_case in range(1, T + 1): 
-    N, number = list(input().split())
+for test_case in range(1, T + 1):
+    N, numbers = map(str, input().split())
+
     answer = ''
-    
-    for num in number:
-        binary = format(int(num, 16), '04b')
-        answer += binary
-        
+    for num in numbers:
+        answer += format(int(num, 16), '04b')
+
     print(f'#{test_case} {answer}')
