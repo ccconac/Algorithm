efe3a07c20a4a61ffe4152379ccbe0ab66356f5f
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 23:25:17 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 회전, Time: 67 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
+++ SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
@@ -1,10 +1,17 @@
+from collections import deque
+
 T = int(input())
 
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
     numbers = list(map(int, input().split()))
 
-    for _ in range(1, M + 1):
-        numbers.append(numbers.pop(0))
+    queue = deque(numbers)
 
-    print(f'#{test_case} {numbers[0]}')
+    # 1. 맨 앞의 숫자를 맨 뒤로 보내는 작업 M번 수행
+    # 2. 맨 앞에 있는 숫자 출력
+    for _ in range(M):
+        queue.append(queue.popleft())
+
+    answer = queue[0]
+    print(f'#{test_case} {answer}')
