4fd304b5136e3588f67be61a49ee7a11e26ae9da
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 20:35:27 +0900

    [D2] Title: 시각 덧셈, Time: 60 ms, Memory: 51,584 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -2,25 +2,19 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     H1, M1, H2, M2 = map(int, input().split())
-    '''
-    시 분으로 이루어진 시각을 2개 입력 받아, 더한 값을 시 분으로 출력하는 프로그램을 작성하라.
-	(시: 12시간제, 1-12)
-    '''
-    time = [0, 0]
-    
-    # 1. H1 + H2 > 12 => H1 + H2 - 12
-    # 2. M1 + M2 > 60 => M1 + M2 - 60, H + 1
+
+    # 시: 1 ~ 12, 분: 0 ~ 59
+    H, M = 0, 0
+
     if H1 + H2 > 12:
-        time[0] = H1 + H2 - 12
+        H = H1 + H2 - 12
     else:
-        time[0] = H1 + H2
-    if M1 + M2 >= 60:
-        time[0] += 1
-        time[1] = M1 + M2 - 60
+        H = H1 + H2
+
+    if M1 + M2 > 59:
+        H += 1
+        M = M1 + M2 - 60
     else:
-        time[1] = M1 + M2
-    
-    answer = ' '.join(map(str, time))
-    print(f'#{test_case} {answer}')
-    
-        
+        M = M1 + M2
+
+    print(f'#{test_case} {H} {M}')
