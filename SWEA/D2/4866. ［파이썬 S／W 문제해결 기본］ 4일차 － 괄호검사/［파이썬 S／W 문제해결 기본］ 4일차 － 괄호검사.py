96dc8faa0fbdffb04b9aed5560e3a0b9b2612019
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 16:34:03 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 4일차 - 괄호검사, Time: 60 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/4866. ［파이썬 S／W 문제해결 기본］ 4일차 － 괄호검사/［파이썬 S／W 문제해결 기본］ 4일차 － 괄호검사.py
+++ SWEA/D2/4866. ［파이썬 S／W 문제해결 기본］ 4일차 － 괄호검사/［파이썬 S／W 문제해결 기본］ 4일차 － 괄호검사.py
@@ -2,20 +2,22 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     strings = input()
-    mapping = {')': '(', '}': '{'} 
+    mapping = {'}': '{', ')': '('}
+
     stack = []
     answer = 1
-    
-    # 1. {, ( 일 경우 스택에 넣음
-    # 2. }, ) 일 경우 스택의 가장 최상단에 있는 것과 비교 후 일치하면 제거
+
     for c in strings:
-        if c == '(' or c =='{': stack.append(c)
-        elif c == ')' or c =='}': 
-            if stack and stack[-1] == mapping[c]: stack.pop()
-            else: 
+        if c == '(' or c == '{':
+            stack.append(c)
+        elif c == ')' or c == '}':
+            if stack and stack[-1] == mapping[c]:
+                stack.pop()
+            else:
                 answer = 0
                 break
-    
-    if stack: answer = 0
-    
+
+    if len(stack):
+        answer = 0
+
     print(f'#{test_case} {answer}')
