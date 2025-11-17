3e216dbeeb721ddca10a955b6a7d1ba6dc4f07a4
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 16:12:39 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 문자열 비교, Time: 59 ms, Memory: 51,072 KB -BaekjoonHub

--- SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
+++ SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
@@ -3,18 +3,15 @@ T = int(input())
 for test_case in range(1, T + 1):
     str1 = input()
     str2 = input()
-    
-    N, M = len(str1), len(str2)
-    '''
-    문자열 str2 안에 str1과 일치하는 부분 있는지
-    첫 문자열이 두번째에 존재하면 1, 존재하지 않으면 0을 출력
-    '''
+
+    str1_length, str2_length = len(str1), len(str2)
     answer = 0
-    
-    for i in range(M - N + 1):
-        word = str2[i:i+N]
-        if word == str1: answer = 1
-  	
+
+    for i in range(str2_length - str1_length + 1):
+        temp = str2[i:i+str1_length]
+
+        if str1 == temp:
+            answer = 1
+            break
+
     print(f'#{test_case} {answer}')
-        
-    
