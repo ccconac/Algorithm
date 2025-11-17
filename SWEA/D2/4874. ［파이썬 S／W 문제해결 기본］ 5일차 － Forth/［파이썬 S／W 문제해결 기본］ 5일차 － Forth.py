7a4df3037c40f3bc9369b8f05794eb0215c653b6
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 17:58:16 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - Forth, Time: 60 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/4874. ［파이썬 S／W 문제해결 기본］ 5일차 － Forth/［파이썬 S／W 문제해결 기본］ 5일차 － Forth.py
+++ SWEA/D2/4874. ［파이썬 S／W 문제해결 기본］ 5일차 － Forth/［파이썬 S／W 문제해결 기본］ 5일차 － Forth.py
@@ -1,32 +1,40 @@
 T = int(input())
+operator = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x // y
+}
 
 for test_case in range(1, T + 1):
-    forth = list(input().split())
+    problem = list(input().split())
     stack = []
-    isError = False
-    operators = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y,
-        '/': lambda x, y: x // y,
-    }
-
     answer = ''
 
-    for command in forth:
-        if command.isdigit(): stack.append(int(command))
-        elif command in operators:
-            if len(stack) < 2:
-                isError = True
-                break
-                
-            b = stack.pop()
-            a = stack.pop()
-            stack.append(operators[command](a, b))
+    # 1. 숫자라면 스택에 넣음
+    # 2. 연산자를 만난다면
+    # 2-1. 스택에서 숫자 두 개를 꺼냄
+    # 2-2. 연산을 함
+    # 2-3. 결과를 다시 스택에 넣음
+    # 2-4. 단, 형식이 잘못되어 계산이 불가능하다면 error 처리
+    # 3. 온점이라면 숫자 꺼내 출력
+    for command in problem:
+        if command.isdigit():
+            stack.append(command)
         elif command == '.':
-            if len(stack) == 1: answer = stack.pop()
-            else: isError = True
-        else: isError = True
+            if len(stack) == 1:
+                answer = stack.pop()
+            else:
+                answer = 'error'
+        else:
+            if len(stack) < 2:
+                answer = 'error'
+                break
 
-    if isError: answer = 'error'
+            b = int(stack.pop())
+            a = int(stack.pop())
+
+            result = operator[command](a, b)
+            stack.append(result)
+
     print(f'#{test_case} {answer}')
