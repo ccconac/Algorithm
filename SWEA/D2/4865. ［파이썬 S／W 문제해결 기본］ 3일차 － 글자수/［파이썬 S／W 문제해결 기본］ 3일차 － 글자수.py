aa1aaac5f2a2535500b598c09775d111082051f9
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-17 16:24:37 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 글자수, Time: 60 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D2/4865. ［파이썬 S／W 문제해결 기본］ 3일차 － 글자수/［파이썬 S／W 문제해결 기본］ 3일차 － 글자수.py
+++ SWEA/D2/4865. ［파이썬 S／W 문제해결 기본］ 3일차 － 글자수/［파이썬 S／W 문제해결 기본］ 3일차 － 글자수.py
@@ -3,12 +3,16 @@ T = int(input())
 for test_case in range(1, T + 1):
     str1 = input()
     str2 = input()
-    
+
     dictionary = {}
-    
-    for c1 in str1: dictionary[c1] = 0
-    for c2 in str2:
-        if c2 in dictionary: dictionary[c2] += 1
+
+    for c in str1:
+        dictionary[c] = 0
+
+    for c in str2:
+        if c in dictionary:
+            dictionary[c] += 1
 
     answer = max(dictionary.values())
+
     print(f'#{test_case} {answer}')
