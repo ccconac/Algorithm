3b5d39bc902980612be930d777adc2fb930bb231
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-19 15:56:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,22 +1,25 @@
+from collections import deque
+
 def bfs(node):
-    queue = [node]
+    queue = deque([node])
+    visited[node] = 1
 
     while queue:
-        current = queue.pop(0)
+        current = queue.popleft()
 
         if current == K:
-            print(visited[current])
-            break
+            return
 
-        for next in (current - 1, current + 1, current * 2):
+        for next in (current + 1, current - 1, current * 2):
             if 0 <= next <= MAX and not visited[next]:
                 visited[next] = visited[current] + 1
                 queue.append(next)
-
-
 
 N, K = map(int, input().split())
 MAX = 10 ** 5
 visited = [0] * (MAX + 1)
 
 bfs(N)
+answer = visited[K] - 1
+
+print(answer)
