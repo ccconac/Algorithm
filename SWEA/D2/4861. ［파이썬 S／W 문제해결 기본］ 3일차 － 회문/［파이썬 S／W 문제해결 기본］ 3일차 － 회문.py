1bf92f677e027c4a06553a2b38a64ae56b345e15
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-22 06:42:00 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 회문, Time: 90 ms, Memory: 61,056 KB -BaekjoonHub

--- SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
+++ SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
@@ -1,32 +1,29 @@
 T = int(input())
 
+def is_palindrome(word):
+    return word == word[::-1]
+
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
-    words = [input() for _ in range(N)]
-    answer = ''
+    words = [list(map(str, input().strip())) for _ in range(N)]
+    t_words = list(map(list, zip(*words)))
+    result = ''
 
-    # 행 먼저 탐색
-    for row in words:
-        for i in range(N - M + 1):
-            candidate = row[i:i + M]
-            if candidate == candidate[::-1]:
-                answer = candidate
+    for i in range(N):
+        for j in range(N - M + 1):
+            word = words[i][j:j+M]
+            t_word = t_words[i][j:j+M]
+
+            if is_palindrome(word):
+                result = word
                 break
 
-        if answer:
+            if is_palindrome(t_word):
+                result = t_word
+                break
+
+        if result:
             break
 
-    # 행 탐색에서 회문 찾지 못했다면, 열 탐색
-    if not answer:
-        for col in range(N):
-            col_strings = ''.join([words[row][col] for row in range(N)])
-            for i in range(N - M + 1):
-                candidate = col_strings[i:i + M]
-                if candidate == candidate[::-1]:
-                    answer = candidate
-                    break
-
-            if answer:
-                break
-
+    answer = ''.join(result)
     print(f'#{test_case} {answer}')
