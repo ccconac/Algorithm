4414c6bc64bf5142b80eef2e19b17b4f02c2b21a
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-21 22:37:09 +0900

    [D3] Title: 농작물 수확하기, Time: 115 ms, Memory: 61,312 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -7,8 +7,9 @@ for test_case in range(1, T + 1):
 
     for i in range(N // 2):
         answer += sum(farm[i][N // 2 - i:N // 2 + i + 1])
-        answer += sum(farm[N - i - 1][N // 2 - i:N // 2 + i + 1])
 
-    answer += sum(farm[N//2])
-    
+        temp = N // 2 + i + 1
+        answer += sum(farm[temp][N // 2 - (N - 1 - temp):N // 2 + (N - temp)])
+
+    answer += sum(farm[N // 2])
     print(f'#{test_case} {answer}')
