6ea6b2d346ba570b1883aa0d2a0371dc577df190
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-21 20:25:11 +0900

    [D3] Title: 원 안의 점, Time: 122 ms, Memory: 59,264 KB -BaekjoonHub

---
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    answer = 0
+
+    for x in range(-N, N + 1):
+        for y in range(-N, N + 1):
+            if x ** 2 + y ** 2 <= N ** 2:
+                answer += 1
+
+    print(f'#{test_case} {answer}')
