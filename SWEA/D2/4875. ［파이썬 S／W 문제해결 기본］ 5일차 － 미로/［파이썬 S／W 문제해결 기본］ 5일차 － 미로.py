b0cc26494f74fa3ee653f40044d37d620c18f194
sohee <106158901+ccconac@users.noreply.github.com>
2025-11-22 11:52:32 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 미로, Time: 82 ms, Memory: 59,904 KB -BaekjoonHub

--- SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
+++ SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
@@ -1,35 +1,36 @@
-T = int(input())
-dir = [[-1, 0], [1, 0], [0, -1], [0, 1]] # 상하좌우
+def dfs(x, y):
+    stack = [(x, y)]
+    visited[x][y] = 1
 
-def dfs():
     while stack:
-        x, y = stack.pop()
-        visited[x][y] = 1
-
+        cx, cy = stack.pop()
         for dx, dy in dir:
-            nx = x + dx
-            ny = y + dy
+            nx = cx + dx
+            ny = cy + dy
 
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 if maze[nx][ny] == 3:
                     return 1
+
                 elif not maze[nx][ny]:
-                    stack.append((nx, ny))
                     visited[nx][ny] = 1
+                    stack.append((nx, ny))
 
     return 0
+
+T = int(input())
+dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 for test_case in range(1, T + 1):
     N = int(input())
     maze = [list(map(int, input().strip())) for _ in range(N)]
     visited = [[0] * N for _ in range(N)]
-    stack = []
 
+    answer = 0
     for i in range(N):
         for j in range(N):
             if maze[i][j] == 2:
-                stack.append((i, j))
+                answer = dfs(i, j)
                 break
 
-    answer = dfs()
     print(f'#{test_case} {answer}')
