d91b65cc923df0791dff725c0a77774439b2721e
sohee <106158901+ccconac@users.noreply.github.com>
2026-01-09 23:12:29 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 12672 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -9,7 +9,7 @@ class Stack {
 
   pop() {
     if (this.empty()) return -1;
-    else return this.arr.pop();
+    return this.arr.pop();
   }
 
   size() {
@@ -17,37 +17,49 @@ class Stack {
   }
 
   empty() {
-    if (this.arr.length === 0) return 1;
-    else return 0;
+    if (this.size() === 0) return 1;
+    return 0;
   }
 
   top() {
     if (this.empty()) return -1;
-    else return this.arr[this.arr.length - 1];
+    return this.arr[this.size() - 1];
   }
 }
 
-const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const solution = (input, stack) => {
+  const answer = [];
 
-const N = Number(input[0]);
+  input.forEach((command) => {
+    const [cmd, X] = command.split(' ');
+
+    switch (cmd) {
+      case 'push':
+        stack.push(X);
+        break;
+      case 'pop':
+        answer.push(stack.pop());
+        break;
+      case 'size':
+        answer.push(stack.size());
+        break;
+      case 'empty':
+        answer.push(stack.empty());
+        break;
+      case 'top':
+        answer.push(stack.top());
+        break;
+    }
+  });
+
+  return answer.join('\n');
+};
+
+const input = require('fs').readFileSync(0).toString().trim().split('\n');
+
+input.shift();
+
 const stack = new Stack();
+const answer = solution(input, stack);
 
-let answer = [];
-
-for (let i = 1; i <= N; i++) {
-  const [command, value] = input[i].split(" ");
-
-  if (command === "push") {
-    stack.push(value);
-  } else if (command === "pop") {
-    answer.push(stack.pop());
-  } else if (command === "size") {
-    answer.push(stack.size());
-  } else if (command === "empty") {
-    answer.push(stack.empty());
-  } else if (command === "top") {
-    answer.push(stack.top());
-  }
-}
-
-console.log(answer.join("\n"));
+console.log(answer);
