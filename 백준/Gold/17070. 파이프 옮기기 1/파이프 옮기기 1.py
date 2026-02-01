57bf026cbcb7b5e5711f960a54fc36eb92b1ed28
sohee <106158901+ccconac@users.noreply.github.com>
2026-02-01 23:55:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -6,12 +6,9 @@ def solution():
 
     for row in range(1, N):
         for col in range(1, N):
-            # 대각선의 경우 현재 좌표랑 (상, 좌) 비어 있어야 함
             if house[row][col] == 0 and house[row][col - 1] == 0 and house[row - 1][col] == 0:
                 dp[1][row][col] = dp[0][row - 1][col - 1] + dp[1][row - 1][col - 1] + dp[2][row - 1][col - 1]
-
-            # 가로의 경우 현재 좌표만 비어 있으면 됨
-            # 세로의 경우 현재 좌표만 비어 있으면 됨
+                
             if house[row][col] == 0:
                 dp[0][row][col] = dp[0][row][col - 1] + dp[1][row][col - 1]
                 dp[2][row][col] = dp[1][row - 1][col] + dp[2][row - 1][col]
