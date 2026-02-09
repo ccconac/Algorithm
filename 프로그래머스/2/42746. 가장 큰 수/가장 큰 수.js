1902da2e8663ce4232ab5eca1db12318ad0d8e8b
sohee <106158901+ccconac@users.noreply.github.com>
2026-02-09 23:28:25 +0900

    [level 2] Title: 가장 큰 수, Time: 170.84 ms, Memory: 46 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,7 @@
 function solution(numbers) {
-    const answer = numbers.map(num => num.toString())
-                               .sort((a, b) => (b + a) - (a + b)).join('');
+    // 0 또는 양의 정수가 주어졌을 때
+    // 정수를 이어 붙여 만들 수 있는 가장 큰 수
+    const answer = numbers.map((number) => number.toString()).sort((a, b) => (b + a) - (a + b));
     
-    return answer[0] === "0" ? "0" : answer;
+    return answer[0] === '0' ? '0' : answer.join('');
 }
