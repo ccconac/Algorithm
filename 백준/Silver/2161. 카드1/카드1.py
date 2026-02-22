1282b31986a760b27370c4ed6ac8fe64074f6678
sohee <106158901+ccconac@users.noreply.github.com>
2026-02-22 23:57:51 +0900

    [Silver V] Title: 카드1, Time: 92 ms, Memory: 109412 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+cards = []
+answer = []
+
+for i in range(1, N + 1):
+    cards.append(i)
+
+while (len(cards) != 0):
+    answer.append(cards.pop(0))
+    
+    if (len(cards) != 0):
+        cards.append(cards.pop(0))
+
+for j in answer:
+    print(j, end =" ")
